2a6d1f219f53000b16c6752f1551c4194661d722
thedanisaur <definisdan@yahoo.com>
2022-12-15 16:18:37 -0500

    Adding quasar frontend

---
+++ frontend/src/pages/RankingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-label>
+      Ranking!
+    </q-label>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'RankingPage'
+})
+</script>
